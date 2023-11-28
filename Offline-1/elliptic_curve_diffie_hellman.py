0353b12e770934207c49770791e62154e16560ea
Md. Nafiu Rahman <nafiu.rahman@gmail.com>
2023-11-29 01:07:56 +0600

    bhul ase thik kora lagbe

--- Offline-1/elliptic_curve_diffie_hellman.py
+++ Offline-1/elliptic_curve_diffie_hellman.py
@@ -6,36 +6,13 @@ import hashlib
 import sys
 import sympy 
 
-#generate prime number of 128 bits
-def generate_prime_number():
-    prime = sympy.randprime(pow(2,127),pow(2,128))
-    return prime
 
 # define the curve
-a = 1
-b = 6
-p = 11
-
-# find the points on the curve
-
-def find_points():
-    points = []
-    for x in range(0, p):
-        y = (x**3 + a*x + b) % p
-        points.append((x, y))
-    return points
-
-# find the generator points
-
-def find_generator_points():
-    points = find_points()
-    generator_points = []
-    for point in points:
-        if point[1] == 0:
-            continue
-        else:
-            generator_points.append(point)
-    return generator_points
+a = 0xfffffffdfffffffffffffffffffffffc
+b = 0xe87579c11079f43dd824993c2cee5ed3
+p = 0xfffffffdffffffffffffffffffffffff
+n = 0xfffffffe0000000075a30d1b9038a115
+G = (0x161ff7528b899b2d0c28607ca52c5b86, 0xcf5ac8395bafeb13c02da292dded7a83)
 
 # point addition
 
@@ -56,12 +33,24 @@ def scalar_multiplication(scalar, point):
     binary = bin(scalar)[2:]
     result = point
     for bit in binary[1:]:
-        result = point_addition(result, result)
+        result = point_addition(result, result) 
         if bit == '1':
             result = point_addition(result, point)
     return result
 
-# prime=generate_prime_number()
-#check if the number is prime
-# if sympy.isprime(prime):
-#     print("Prime number is: ",prime)
+#generate a random number between 2^(128-1) and n-1
+k_prA = random.randint(pow(2,127),n-1)
+
+k_prB = random.randint(pow(2,127),n-1)
+
+# public key generation
+k_pbA = scalar_multiplication(k_prA, G)
+k_pbB = scalar_multiplication(k_prB, G)
+
+# shared key generation
+k_sA = scalar_multiplication(k_prA, k_pbB)
+k_sB = scalar_multiplication(k_prB, k_pbA)
+
+# print the shared key
+print("Shared Key: ",k_sA[0])
+print("Shared Key: ",k_sB[0])
